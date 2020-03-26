527f2fa5c4e1c7d48dc553290d727b0e9f7078b0
Matt Collier <matt.collier@hotmail.com>
2020-03-26 09:59:54 +0000

    add fetch KPI test

---
+++ tests/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from '../../src/components/App';
+import { shallow } from 'enzyme';
+
+global.fetch = require('jest-fetch-mock');
+
+describe('Fetch KPIs', () => {
+    test('retrieves all KPIs from the server', () => {
+    });
+  });
