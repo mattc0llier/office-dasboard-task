ccb84b53bbe17785784b73d199f03c588bba7962
Matt Collier <matt.collier@hotmail.com>
2020-03-26 10:07:08 +0000

    added card component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Card';
+
+const Dashboard = () => {
+    return(
+        <div className="dashboard">
+            <h3>Dashboard</h3>
+            <Card />
+        </div>
+        
+    )
+}
+
+export default Dashboard
