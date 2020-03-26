f64058c7af9a4611e456d0cfa7a042e078800ce6
Matt Collier <matt.collier@hotmail.com>
2020-03-26 11:12:25 +0000

    kpi components loaded based on their id

--- src/components/Card.js
+++ src/components/Card.js
@@ -17,19 +17,67 @@ class Card extends React.Component {
 
     render(){
         console.log('kpi id', this.props.kpi)
-        return(
-            <div className='card'>
-                <div className='card-container'>
-                    <h3>{this.props.kpi.title}</h3>
-                    <ConcurrentUsers />
-                    <OfficeTemperature />
-                    <PlantReminder />
-                    <OutsideWeather />
-                    <DrinksLeft />
-                    <WhosInTheOffice />
-                </div>
-            </div>          
-        )
+        if (this.props.kpi.id == 1) {	
+            return(	
+                <div className='card'>	
+                    <div className='card-container'>	
+                        <h3>{this.props.kpi.title}</h3>	
+                        <p className='kpi-text'>{this.props.kpi.data}</p>	
+                        <ConcurrentUsers />	
+                    </div>	
+                </div>          	
+            )	
+        } else if (this.props.kpi.id == 2) {	
+            return(	
+                <div className='card'>	
+                    <div className='card-container'>	
+                        <h3>{this.props.kpi.title}</h3>	
+                        <p className='kpi-text'>{this.props.kpi.data.currentTemperature}</p>	
+                        <OfficeTemperature />	
+                    </div>	
+                </div>          	
+            )	
+        } else if (this.props.kpi.id == 3) {	
+            return(	
+                <div className='card'>	
+                    <div className='card-container'>	
+                        <h3>{this.props.kpi.title}</h3>	
+                        <p className='kpi-text'>{this.props.kpi.data.nextWaterDate}</p>	
+                        <PlantReminder />	
+                    </div>	
+                </div>          	
+            )	
+        } else if (this.props.kpi.id == 4) {	
+            return(	
+                <div className='card'>	
+                    <div className='card-container'>	
+                        <h3>{this.props.kpi.title}</h3>	
+                        <p className='kpi-text'>{this.props.kpi.data.temperature}</p>	
+                        <OutsideWeather />	
+                    </div>	
+                </div>          	
+            )	
+        } else if (this.props.kpi.id == 5) {	
+            return(	
+                <div className='card'>	
+                    <div className='card-container'>	
+                        <h3>{this.props.kpi.title}</h3>	
+                        <p className='kpi-text'>{this.props.kpi.data.drinksRemaining}</p>	
+                        <DrinksLeft />	
+                    </div>	
+                </div>          	
+            )	
+        } else if (this.props.kpi.id == 6) {	
+            return(	
+                <div className='card'>	
+                    <div className='card-container'>	
+                        <h3>{this.props.kpi.title}</h3>	
+                        <p className='kpi-text'>{this.props.kpi.data[0].name}</p>	
+                        <WhosInTheOffice />	
+                    </div>	
+                </div>          	
+            )	
+        }
     }
 }
 
