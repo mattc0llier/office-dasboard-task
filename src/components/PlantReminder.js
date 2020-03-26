9da4dce7c1acddc64551f5f04b9af7103dfe8123
Matt Collier <matt.collier@hotmail.com>
2020-03-26 11:33:37 +0000

    made the plant reminder dynamic from todays date

--- src/components/PlantReminder.js
+++ src/components/PlantReminder.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const PlantReminder = ({kpi}) => {
+
+    const today = new Date().getTime();
+    const wateringDate = new Date(kpi.data.nextWaterDate).getTime()
+    const timeUntilWater = Math.abs(wateringDate - today)
+    const diff = Math.floor(timeUntilWater/(1000 * 60 * 60  * 24))
+
     return(
-        <p className="kpi-text">{kpi.data.nextWaterDate}</p>
+    <p className='kpi-text'>{diff} 
+        <span className='units-text'>days</span>
+    </p>
     )
 }
 
-export default PlantReminder
+export default  PlantReminder
